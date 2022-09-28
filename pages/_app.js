8be60aa04c4104ec7705acfbe09958c03f72822e
Kartik0899 <jaybudhraja08@gmail.com>
2022-09-28 17:39:00 +0530

    Update script again

--- pages/_app.js
+++ pages/_app.js
@@ -7,22 +7,22 @@ import * as ga from '../lib/google-analytics'
 
 function MyApp({ Component, pageProps }) {
 
-  const router = useRouter()
+  // const router = useRouter()
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-    router.events.on('routerChnageComplete', handleRouteChange)
-    return () => {
-      router.events.off('routerChnageComplete', handleRouteChange)
-    }
-  }, [router.events])
+  // useEffect(() => {
+  //   const handleRouteChange = (url) => {
+  //     ga.pageview(url)
+  //   }
+  //   router.events.on('routerChnageComplete', handleRouteChange)
+  //   return () => {
+  //     router.events.off('routerChnageComplete', handleRouteChange)
+  //   }
+  // }, [router.events])
 
   return (
     <>
       {/* <!-- Google tag (gtag.js) --> */}
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} strategy='afterInteractive'></Script>
+      {/* <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} strategy='afterInteractive'></Script>
       <Script id='google-analytics-script' strategy='afterInteractive'>
         {`
         window.dataLayer = window.dataLayer || [];
@@ -31,6 +31,21 @@ function MyApp({ Component, pageProps }) {
 
         gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
         `}
+      </Script> */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-G9K1637SCD`}
+      />
+
+      <Script strategy="lazyOnload" id='google-analytics-script'>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G9K1637SCD', {
+              page_path: window.location.pathname,
+            });
+                `}
       </Script>
       <Component {...pageProps} />
     </>
